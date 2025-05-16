16ab9c72d8f8d60b8b607160e6886a60da93f647
Jonathan de Gaston <developerjon5@gmail.com>
2025-05-16 16:03:48 -0600

    update package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.swipe_clean"
+    namespace = "com.degastonapps.swipe_clean"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.swipe_clean"
+        applicationId = "com.degastonapps.swipe_clean"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
